8a6fb4ca70b0384d93f09b131de85c811b0b419c
unknown <guan.lefu@opg.cn>
2025-01-17 18:37:31 +0800

    Update

--- src/main/java/com/cat/coin/coincatmanager/framework/ResAdvice.java
+++ src/main/java/com/cat/coin/coincatmanager/framework/ResAdvice.java
@@ -3,7 +3,6 @@ package com.cat.coin.coincatmanager.framework;
 
 import com.cat.coin.coincatmanager.domain.pojo.Page;
 import com.cat.coin.coincatmanager.utils.Resp;
-import com.sun.istack.internal.NotNull;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -22,11 +21,11 @@ public class ResAdvice implements ResponseBodyAdvice<Object> {
 
     @Override
     public Object beforeBodyWrite(Object body,
-                                  @NotNull MethodParameter returnType,
-                                  @NotNull MediaType selectedContentType,
-                                  @NotNull Class<? extends HttpMessageConverter<?>> selectedConverterType,
-                                  @NotNull ServerHttpRequest request,
-                                  @NotNull ServerHttpResponse response) {
+                                   MethodParameter returnType,
+                                  MediaType selectedContentType,
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request,
+                                  ServerHttpResponse response) {
         if(body == null){
             return Resp.success("");
         }
